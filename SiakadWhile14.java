ca6f59722d006b251f51e376bf6c0323e7d2491d
daspro-obsheet2 <wildanwibisono52@gmail.com>
2024-10-10 20:43:21 +0700

    Modifikasi percobaan 2 nomer 4

--- SiakadWhile14.java
+++ SiakadWhile14.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SiakadWhile16 {
+public class SiakadWhile14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int nilai;
